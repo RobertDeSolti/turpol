6b48da7ac0c86669e525560e9d53ae621003a5ee
RobertDeSolti <hredil.r@gmail.com>
2021-02-19 12:04:53 +0100

    1

--- src/index.js
+++ src/index.js
@@ -1,27 +1,76 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from 'react';
+import ReactDOM from 'react-dom';
 
 // CSS
-import './index.scss'
-function BookList() {
-  return (
-    <section className='booklist'>
-      <Book />
-    </section>
-  )
-}
-const author = 'Amelia Hepworth'
-const Book = () => {
-  const tittle = 'I Love You to the Moon and Back'
-  return (
-    <article className='book'>
-      <img src='img/b3_cracow.jpg' alt='b3_cracow.jpg' />
-      <h1>{tittle}</h1>
-      <h4>{author.toUpperCase()}</h4>
-      {/*<p>{let x=6}</p>*/}
-      <p>{6 + 6}</p>
-    </article>
-  )
-}
+import './index.scss';
+// setup vars
+const firstCard = {
+	tittle: 'ZAKOPANE',
+	img: 'img/b5_zakopane.jpg'
+};
+const secondCard = {
+  tittle: 'POZNAN',
+  img: 'img/b5_poznan.jpg'
+};
+const thirdCard = {
+  tittle: 'CRACOW',
+  img: 'img/b5_cracow.jpg'
+};
+const fourthCard = {
+  tittle: 'WARSAW',
+  img: 'img/b5_warsaw.jpg'
+};
+const fifthCard = {
+  tittle: 'JAWORKI',
+  img: 'img/b5_jaworki.jpg'
+};
+const sixthCard = {
+  tittle: 'GDANSK',
+  img: 'img/b5_gdansk.jpg'
+};
 
-ReactDOM.render(<BookList />, document.getElementById('root'))
+const like_icon = 'img/svg/like.svg';
+const comm_icon = 'img/svg/comm.svg';
+const content = 'Jump off balcony, onto strangers head. Chase ball of string hide when guests come over. Being gorgeous with belly side up i could pee on this.';
+
+function SectionToplist() {
+  return (
+    <section className='section-toplist__posts'>
+      <Postcard img={firstCard.img} tittle={firstCard.tittle} />
+      <Postcard img={secondCard.img} tittle={secondCard.tittle} />
+      <Postcard img={thirdCard.img} tittle={thirdCard.tittle} />
+		<Postcard img={fourthCard.img} tittle={fourthCard.tittle} />
+		<Postcard img={fifthCard.img} tittle={fifthCard.tittle} />
+		<Postcard img={sixthCard.img} tittle={sixthCard.tittle} />
+    </section>
+  );
+};
+
+const Postcard = (props) => {
+	return (
+		<div className="postcard">
+			<div className="postcard__tittle">
+				<img src={props.img} alt="b5_zakopane" className="thumbnail" />
+				<div className="overlay">
+					<h3 className="overlay__tittle">{props.tittle}</h3>
+					<div className="overlay__stats">
+						<div className="likes">
+							<p className="likes__number">221</p>
+							<img src={like_icon} alt="like" className="likes__icon" />
+						</div>
+						<div className="comments">
+							<p className="comments__number">50</p>
+							<img src={comm_icon} alt="comm" className="comments__icon" />
+						</div>
+					</div>
+				</div>
+			</div>
+			<p className="postcard__content">{content}</p>
+			<div className="postcard__button">
+				<button className="see-more">SEE MORE</button>
+			</div>
+		</div>
+	)
+};
+
+ReactDOM.render(<SectionToplist />, document.getElementById('toplist'));
